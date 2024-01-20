0c3e75e0fcca88694e53bc46e6f67618aacc7598
Yogeshzirpe2000 <125539253+Yogeshzirpe2000@users.noreply.github.com>
2024-01-20 15:33:42 +0530

    Add files via upload

---
+++ 2667. Create Hello World Function.js
@@ -0,0 +1,16 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        var s="Hello World";
+        return s;
+        
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
